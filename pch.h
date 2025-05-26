67ff7088b0eb574c66b1d302d2d18f342049b133
schloing <user.datagram.protocol@proton.me>
2025-05-26 19:45:41 +1000

    precompiled headers for cmake

---
+++ pch.h
@@ -0,0 +1,21 @@
+#ifndef NV_PCH
+#define NV_PCH
+
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <float.h>
+#include <locale.h>
+#include <math.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <time.h>
+
+#endif
