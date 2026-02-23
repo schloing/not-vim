c29a71a943097a835d30d21877a7d71bd8706dd5
schloing <user.datagram.protocol@proton.me>
2026-02-23 19:33:19 +1100

    NV_EVENT_COUNT

--- events.h
+++ events.h
@@ -6,6 +6,7 @@ enum nv_event_sub {
     NV_EVENT_BUFFCLOSE,
     NV_EVENT_KEYPRESS,
     NV_EVENT_HOTKEY,
+    NV_EVENT_COUNT,
 };
 
 void nv_register_event(enum nv_event_sub event, const char* name, void (*callback)(void));
