9df33f235c45240f41f915112dcdf8b3756d468e
schloing <user.datagram.protocol@proton.me>
2026-02-24 18:01:29 +1100

    feat: BUFFDRAW and WINDOW_SPLIT event

--- events.c
+++ events.c
@@ -53,6 +53,8 @@ const char* nv_event_str(enum nv_event_sub event)
     switch (event) {
     case NV_EVENT_BUFFLOAD:         return "BUFFLOAD";
     case NV_EVENT_BUFFCLOSE:        return "BUFFCLOSE";
+    case NV_EVENT_BUFFDRAW:         return "BUFFDRAW";
+    case NV_EVENT_WINDOW_SPLIT:     return "WINDOW_SPLIT";
     case NV_EVENT_KEYPRESS:         return "KEYPRESS";
     case NV_EVENT_HOTKEY:           return "HOTKEY";
     default:                        return "UNKNOWN";
