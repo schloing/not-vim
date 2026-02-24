8c8214062a131c1c7d2238e93d3af6e75e92069d
schloing <user.datagram.protocol@proton.me>
2026-02-24 18:36:43 +1100

    plugload test for lua scripting api

--- plugload/main.lua
+++ plugload/main.lua
@@ -1,1 +1,5 @@
-echo "hello\n"
+function buff_redraw_handler(ctx)
+    local buff = ctx:get_buffer()
+    echo(tostring(buff:path()) .. "\n")
+end
+local id = event.subscribe(event.BUFFDRAW, buff_redraw_handler)
