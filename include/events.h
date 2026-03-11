21638f58965aeb90624e8443d066e026e1cea8fb
schloing <user.datagram.protocol@proton.me>
2026-03-11 16:09:34 +1100

    fix: update nv_event_register_sub signature in header

--- include/events.h
+++ include/events.h
@@ -14,8 +14,23 @@ enum nv_event_sub {
     NV_EVENT_COUNT,
 };
 
+enum nv_event_type {
+    NV_EVENT_TYPE_REMOTE_RPC,
+    NV_EVENT_TYPE_LUA_REF,
+    NV_EVENT_TYPE_COUNT,
+};
+
+struct nv_event_callback {
+    enum nv_event_type type;
+
+    union {
+        // TODO: rpc event name, args, channel, etc.
+        int ref;
+    };
+};
+
 const char* nv_event_str(enum nv_event_sub event);
-void nv_event_register_sub(enum nv_event_sub event, int lua_callback_ref);
+void nv_event_register_sub(enum nv_event_sub event, struct nv_event_callback callback);
 enum nv_event_sub nv_str_event(const char* str);
 int nv_event_emit(enum nv_event_sub event, struct nv_context* ctx);
 const bool nv_event_is_valid(enum nv_event_sub event);
