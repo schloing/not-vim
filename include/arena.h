3d92d8dd9fb923fcde6a1a68c3d1f0531df8c70c
schloing <user.datagram.protocol@proton.me>
2026-03-15 15:35:58 +1100

    fix: stage config for use with arena

---
+++ include/arena.h
@@ -0,0 +1,28 @@
+#ifndef NV_ARENA_H
+#define NV_ARENA_H
+
+#include <stddef.h>
+
+struct nv_arena_alloc_head;
+
+enum nv_arena_block_status {
+    NV_ARENA_BLOCK_STATUS_RESERVED,
+    NV_ARENA_BLOCK_STATUS_FREE,
+};
+
+struct nv_arena_alloc_head {
+    size_t size;
+    enum nv_arena_block_status status;
+    struct nv_arena_alloc_head* next;
+};
+
+void nv_arena_init();
+void nv_arena_free_all();
+void nv_arena_free(void* ptr);
+void* nv_arena_malloc(size_t size);
+void* nv_arena_calloc(size_t nmemb, size_t size);
+void* nv_arena_realloc(void* ptr, size_t size);
+
+#define NV_ARENA_DEFAULT_ALLOC_SIZE 1024 * 1024
+
+#endif
---
+++ include/nvtree_conf.h
@@ -0,0 +1,9 @@
+#ifndef NVTREE_CONFIG_H
+#define NVTREE_CONFIG_H
+
+#include "arena.h"
+
+#define NVTREE_MALLOC nv_arena_malloc
+#define NVTREE_FREE nv_arena_free
+
+#endif
